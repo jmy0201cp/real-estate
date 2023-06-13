7b6cf2819250c9403c2a9c8b011f2231970cc7ff
miyoung <jmy0201cp@gmail.com>
2023-06-13 22:59:01 +0900

    front-end 폴더 생성

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> 여기 리액트입니다. </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
