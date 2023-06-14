e35df56373a01ca56b25af5f5bb3ac5bc8118b32
miyoung <jmy0201cp@gmail.com>
2023-06-14 20:38:26 +0900

    (server) 로그인 기능 service 구현

--- server/src/main/java/com/realstate/home/service/MemberService.java
+++ server/src/main/java/com/realstate/home/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.realstate.home.service;
 
+import com.realstate.home.dto.request.MemberLoginRequest;
 import com.realstate.home.dto.request.MemberRequest;
+import com.realstate.home.dto.response.MemberLoginResponse;
 import com.realstate.home.dto.response.MemberResponse;
 
 public interface MemberService {
     MemberResponse signup(MemberRequest request);
+
+    MemberLoginResponse login(MemberLoginRequest request);
 }
