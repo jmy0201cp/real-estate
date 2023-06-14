e7e9d11d860115a25b6f161509f2b418efe21686
jmy0201cp <jmy0201cp@gmail.com>
2023-06-14 17:17:00 +0900

    (server) 비밀번호 암호화 적용과 유저네임 중복 체크

--- server/src/main/java/com/realstate/home/repository/MemberRepository.java
+++ server/src/main/java/com/realstate/home/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import com.realstate.home.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+        Optional<Member> findByMemberName(String memberName);
 }
--- server/src/main/java/com/realstate/home/service/MemberServiceImpl.java
+++ server/src/main/java/com/realstate/home/service/MemberServiceImpl.java
@@ -3,8 +3,11 @@ package com.realstate.home.service;
 import com.realstate.home.domain.entity.Member;
 import com.realstate.home.dto.request.MemberRequest;
 import com.realstate.home.dto.response.MemberResponse;
+import com.realstate.home.exception.ErrorCode;
+import com.realstate.home.exception.HomeException;
 import com.realstate.home.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +15,15 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public MemberResponse signup(MemberRequest request) {
-       Member member = memberRepository.save(Member.of(request));
-       return MemberResponse.fromEntity(member);
+       memberRepository.findByMemberName(request.getMemberName()).ifPresent(it -> {
+            throw new HomeException(ErrorCode.DUPLICATED_DATA);
+        });
+       request.setPassword(encoder.encode(request.getPassword()));
+       Member created = memberRepository.save(Member.of(request));
+       return MemberResponse.fromEntity(created);
     }
 }
