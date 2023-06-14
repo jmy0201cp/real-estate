f533eafcdd30e9898b4b3d8bbed3d4654e0749a0
jmy0201cp <jmy0201cp@gmail.com>
2023-06-14 20:36:17 +0900

    (server) jwt 유틸 작성

---
+++ server/src/main/java/com/realstate/home/util/JwtTokenUtils.java
@@ -0,0 +1,34 @@
+package com.realstate.home.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtTokenUtils {
+
+    public static String createJwtToken(String memberName, String key, long expiredTimeMs) {
+        Claims claims = Jwts.claims();
+
+        //유저를 식별하는 값
+        claims.put("memberName", memberName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                .signWith(getKey(key), SignatureAlgorithm.HS256)
+                .compact();
+        //스트링으로 리턴
+    }
+
+    //키 변환하는 함수
+    private static Key getKey(String key) {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
