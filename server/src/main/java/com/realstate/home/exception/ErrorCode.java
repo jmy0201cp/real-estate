d7ab83cacfdbc55e1bc747b53cb3996d6d6b9818
jmy0201cp <jmy0201cp@gmail.com>
2023-06-14 20:38:26 +0900

    (server) 로그인 기능 service 구현

--- server/src/main/java/com/realstate/home/exception/ErrorCode.java
+++ server/src/main/java/com/realstate/home/exception/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     DO_NOT_FOUND(HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND),
+    PASSWORD_IS_NOT_VALID(HttpStatus.CONFLICT),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR),
     DUPLICATED_DATA(HttpStatus.CONFLICT);
 
