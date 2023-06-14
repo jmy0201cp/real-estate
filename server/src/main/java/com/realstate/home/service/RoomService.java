58716ab30785b210f371791fbce340e397e61298
miyoung <jmy0201cp@gmail.com>
2023-06-15 01:39:48 +0900

    (server) 방 사진파일 업로드 서비스 구현 & yml 작성

--- server/src/main/java/com/realstate/home/service/RoomService.java
+++ server/src/main/java/com/realstate/home/service/RoomService.java
@@ -2,12 +2,15 @@ package com.realstate.home.service;
 
 import com.realstate.home.dto.request.RoomRequest;
 import com.realstate.home.dto.response.RoomResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface RoomService {
 
     RoomResponse create(RoomRequest request);
+
+    void saveFile(Long roomId, MultipartFile file);
 
     RoomResponse getRoomById(Long roomId);
 
--- server/src/main/java/com/realstate/home/service/RoomServiceImpl.java
+++ server/src/main/java/com/realstate/home/service/RoomServiceImpl.java
@@ -1,14 +1,23 @@
 package com.realstate.home.service;
 
+import com.realstate.home.domain.entity.File;
 import com.realstate.home.domain.entity.Room;
 import com.realstate.home.dto.request.RoomRequest;
 import com.realstate.home.dto.response.RoomResponse;
 import com.realstate.home.exception.ErrorCode;
 import com.realstate.home.exception.HomeException;
+import com.realstate.home.repository.FileRepository;
 import com.realstate.home.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,10 +26,45 @@ import java.util.stream.Collectors;
 public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
+    private final FileRepository fileRepository;
+
+    @Value("${spring.servlet.multipart.location}")
+    private String uploadPath;
+
     @Override
     public RoomResponse create(RoomRequest request) {
         Room room = roomRepository.save(Room.of(request));
         return RoomResponse.fromEntity(room);
+    }
+
+    @Override
+    public void saveFile(Long roomId, MultipartFile file) {
+        //해당 아이디의 방이 존재하는지
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> {
+           throw new HomeException(ErrorCode.DO_NOT_FOUND);
+        });
+
+        try {
+            String roomPath = uploadPath.concat("/" + roomId);
+            Path dirPath = Paths.get(roomPath);
+
+            if(!Files.exists(dirPath)) {
+                Files.createDirectory(dirPath);
+            }
+
+            InputStream inputSteam = file.getInputStream();
+
+            Files.copy(inputSteam, dirPath.resolve(file.getOriginalFilename()),
+                    StandardCopyOption.REPLACE_EXISTING);
+
+            fileRepository.save(File.of(room.getRoomId(), file.getOriginalFilename(), roomPath));
+            inputSteam.close();
+
+
+        } catch (Exception e) {
+            throw new HomeException(ErrorCode.SERVER_ERROR);
+        }
+
     }
 
     @Override
