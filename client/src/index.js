61e53f36436afec153dde676901fa312d500f100
miyoung <jmy0201cp@gmail.com>
2023-06-17 02:23:08 +0900

    (client) 라우터 이용 작성

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Main from "./pages/Main";
+import RoomDetail from "./pages/RoomDetail";
+import RoomContainer from "./pages/RoomContainer";
+import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Main /> },
+      { path: "/rooms", element: <RoomContainer /> },
+      { path: "/rooms/:roomId", element: <RoomDetail /> },
+    ],
+  },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <RouterProvider router={router} />
+  </>
 );
