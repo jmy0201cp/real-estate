4e91ef0631bf7bf5e31f2dec799e0c30cea15a7d
jmy0201cp <jmy0201cp@gmail.com>
2023-06-21 02:33:07 +0900

    (client) 필요없는 페이지 삭제

--- client/src/pages/Bascket.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Bascket() {
-  return (
-    <div>
-      <h1>찜 바구니</h1>
-    </div>
-  );
-}
--- client/src/pages/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Main() {
-  return (
-    <div>
-      <h1>메인 페이지</h1>
-    </div>
-  );
-}
