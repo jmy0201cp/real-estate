919a91c61553f88746a2bbaa50d4ed2455861abd
miyoung <jmy0201cp@gmail.com>
2023-06-21 02:44:18 +0900

    (server) enum 타입 소문자로 자동 변경되도록 config 추가

---
+++ server/src/main/java/com/realstate/home/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.realstate.home.config;
+
+import com.realstate.home.dto.RoomTypeConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new RoomTypeConverter());
+    }
+}
