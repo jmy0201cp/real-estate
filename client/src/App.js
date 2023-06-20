b640de010f67a38bc6e9894d53c705813888fcd7
miyoung <jmy0201cp@gmail.com>
2023-06-21 02:29:30 +0900

    (client) tailwind css 적용 및 로고 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <div className="bg-[#e0dfe4] flex flex-col justify-end items-center h-screen">
+      <img
+        src="/image/logo.png"
+        alt=""
+        className="h-72 absolute top-[-57px] cursor-pointer"
+        onClick={() => navigate("/")}
+      />
+      <div className="bg-[#f9f8fd] w-2/3 rounded-t-[30px] h-4/5 overflow-hidden z-10">
+        <Header />
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
