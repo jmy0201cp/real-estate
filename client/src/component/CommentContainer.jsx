9c5e92f3e4f44620f8609644c244543afbbeb10f
jmy0201cp <jmy0201cp@gmail.com>
2023-06-22 18:06:45 +0900

    (client) 문의 댓글 container 생성

---
+++ client/src/component/CommentContainer.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { AiTwotoneEdit } from "react-icons/ai";
+import CommentForm from "./CommentForm";
+import Comment from "./Comment";
+import { useParams } from "react-router-dom";
+
+export default function InquiryComment() {
+  const { roomId } = useParams();
+  const [comments, setComments] = useState([]);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  useEffect(() => {
+    fetch(`/rooms/${roomId}/comment`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((data) => data.json())
+      .then((res) => setComments(res.data));
+  }, []);
+
+  return (
+    <div className="rounded-lg shadow-md bg-white overflow-hidden cursor-pointer w-1/3 h-[365px] pt-2 border border-lightgray float-right p-2 mr-5 mb-5 items-center">
+      <div>
+        <div className="flex items-center justify-center">
+          <AiTwotoneEdit />
+          <p className="text-center pt-3 mb-4 ml-2 text-xs text-gray-400">
+            해당 상품에 대한 문의를 남겨주세요.
+          </p>
+        </div>
+        {!token && (
+          <p className="text-[7px] text-right mr-2">
+            댓글 기능은 로그인 후 사용 가능합니다
+          </p>
+        )}
+      </div>
+      <CommentForm />
+      <ul className="my-2 w-full max-h-[255px] overflow-auto">
+        {comments.map((comment) => (
+          <Comment key={comment.commentId} comment={comment} />
+        ))}
+      </ul>
+    </div>
+  );
+}
