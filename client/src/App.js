73f6dc019a89f1dcfe98c03cf25bb5b3813d1b3c
miyoung <jmy0201cp@gmail.com>
2023-06-23 02:53:56 +0900

    (client) token 필요한 모든 컴포넌트에게 우산 씌워주기

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "./component/Header";
+import { LoginTokenProvider } from "./context/LoginTokenContext";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-[#e0dfe4] flex flex-col justify-end items-center h-screen">
-      <img
-        src="/image/logo.png"
-        alt=""
-        className="h-72 absolute top-[-57px] cursor-pointer"
-        onClick={() => navigate("/")}
-      />
-      <div className="bg-[#f9f8fd] w-2/3 rounded-t-[30px] h-4/5 overflow-hidden z-10">
-        <Header />
-        <Outlet />
+    <LoginTokenProvider>
+      <div className="bg-[#e0dfe4] flex flex-col justify-end items-center h-screen">
+        <img
+          src="/image/logo.png"
+          alt=""
+          className="h-72 absolute top-[-57px] cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+        <div className="bg-[#f9f8fd] w-2/3 rounded-t-[30px] h-4/5 overflow-hidden z-10">
+          <Header />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </LoginTokenProvider>
   );
 }
 
